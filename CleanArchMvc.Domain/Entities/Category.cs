aef4a36358088e068c3a9e6b9a941d9a269e29d6
HeFidelis <heitorfidelis81@gmail.com>
2026-03-02 09:26:10 -0400

    Enriquecendo o modelo de domínio criado - I

--- CleanArchMvc.Domain/Entities/Category.cs
+++ CleanArchMvc.Domain/Entities/Category.cs
@@ -1,14 +1,38 @@
-﻿using System;
+﻿using CleanArchMvc.Domain.Validation;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
 namespace CleanArchMvc.Domain.Entities
 {
-    public class Category
+    public sealed class Category
     {
-        public int Id { get; set; }
-        public string Name { get; set; }
+        public int Id { get; private set; }
+        public string Name { get; private set; }
 
-        public ICollection<Product> Products { get; set; }
+        public Category(string name)
+        {
+            ValidateDomain(name);
+        }
+
+        public Category(int id, string name)
+        {
+            DomainExceptionValidation.When(id < 0, "Invalid Id. Id must be greater than zero.");
+            Id = id;
+            ValidateDomain(name);
+        }
+
+        public ICollection<Product> Products { get; private set; }
+
+        private void ValidateDomain(string name)
+        {
+            DomainExceptionValidation.When(string.IsNullOrEmpty(name),
+                "Invalid name. Name is required.");
+
+            DomainExceptionValidation.When(name.Length < 3,
+                "Invalid name, too short, minimum 3 characters");
+
+            Name = name;
+        }
     }
 }
