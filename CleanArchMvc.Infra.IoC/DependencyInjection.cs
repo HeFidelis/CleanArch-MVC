4b2f6848767d17679a64216b857e923f4ac2b462
HeFidelis <heitorfidelis81@gmail.com>
2026-03-05 15:25:59 -0400

    Teste - Registrando os serviços e criando o Controller e View para Category

--- CleanArchMvc.Infra.IoC/DependencyInjection.cs
+++ CleanArchMvc.Infra.IoC/DependencyInjection.cs
@@ -1,9 +1,14 @@
-﻿using CleanArchMvc.Domain.Interfaces;
+﻿using AutoMapper;
+using CleanArchMvc.Application.Interfaces;
+using CleanArchMvc.Application.Mappings;
+using CleanArchMvc.Application.Services;
+using CleanArchMvc.Domain.Interfaces;
 using CleanArchMvc.Infra.Data.Context;
 using CleanArchMvc.Infra.Data.Repositories;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Logging;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -12,15 +17,25 @@ namespace CleanArchMvc.Infra.IoC
 {
     public static class DependencyInjection
     {
+
         public static IServiceCollection AddInfrastructure(this IServiceCollection services, 
             IConfiguration configuration)
         {
+
             services.AddDbContext<AppDbContext>(options => 
             options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"),
                 b => b.MigrationsAssembly(typeof(AppDbContext).Assembly.FullName)));
 
             services.AddScoped<ICategoryRepository, CategoryRepository>();
             services.AddScoped<IProductRepository, ProductRepository>();
+
+            services.AddScoped<IProductService, ProductService>();
+            services.AddScoped<ICategoryService, CategoryService>();
+
+            services.AddSingleton(provider => new MapperConfiguration(cfg =>
+            {
+                cfg.AddProfile<DomainToDTOMappingProfile>();
+            }).CreateMapper());
 
             return services;
         }
