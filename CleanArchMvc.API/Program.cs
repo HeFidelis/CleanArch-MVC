69b87ac66fd8a5493c43707f300c67f9589fefcc
HeFidelis <heitorfidelis81@gmail.com>
2026-03-26 13:17:45 -0400

    HttpGet - Retornando todas as Categorias

--- CleanArchMvc.API/Program.cs
+++ CleanArchMvc.API/Program.cs
@@ -6,15 +6,16 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
 builder.Services.AddInfrastructureAPI(builder.Configuration);
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
 {
-    app.MapOpenApi();
+    app.UseSwagger();
+    app.UseSwaggerUI();
 }
 
 app.UseHttpsRedirection();
