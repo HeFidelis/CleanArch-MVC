00e2fe32660128fd45ffe90cd085ee848f508556
HeFidelis <heitorfidelis81@gmail.com>
2026-03-27 09:14:57 -0400

    Ativando a autenticação e validando o token JWT

--- CleanArchMvc.API/Program.cs
+++ CleanArchMvc.API/Program.cs
@@ -7,6 +7,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddInfrastructureAPI(builder.Configuration);
+builder.Services.AddInfrastructureJWT(builder.Configuration);
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
